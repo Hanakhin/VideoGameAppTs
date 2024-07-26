b0036a5a5a1e1b3abf1ca23318f8c7417afdcf06
hanakhin <hanakhin.nounimassotte@gmail.com>
2024-07-26 16:21:28 +0200

    removed express,cors,nodemon since it was useless

--- server/express/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-app.listen(3003,()=>{
-    console.log("Server running on port 3003")
-})
